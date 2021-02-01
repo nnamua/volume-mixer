cc38252a6a4ab84bb714aa1a61438f29941b9af9
paumann <paul.aumann@gmail.com>
2021-02-01 14:46:43 +0100

    Documentation for Reset

--- server/Reset.py
+++ server/Reset.py
@@ -1,4 +1,8 @@
 from Arduino import Arduino
 
+"""
+    This script creates and destroys an Arduino object, thus resetting the COM port.
+"""
+
 arduino = Arduino(port="COM6", wait=False)
 del arduino
